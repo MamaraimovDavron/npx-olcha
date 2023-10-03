e53399d2b6f6ca3cbf254f8c1a3beb1bb4dd3316
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-10-03 16:11:37 +0500

    builded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="shadow">
+        <div className="container-fluid py-3">
+          <img src="https://olcha.uz/_nuxt/logo-red.e48e0ab8.svg" alt="" />
+        </div>
       </header>
+
+      <section id="products" className="py-5">
+        <div className="container-fluid">
+          <div className="row">
+            <div className=".col-sm-6 .col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/2022-12-23/noutbuk-lenovo-v15-igl-n4020-4256-gb-ssd-156-hd-185052-0.jpeg"
+                  alt=""
+                />
+                <a href="!#" className="groupName d-block mb-3 mt-3">
+                  Laptops
+                </a>
+                <p className="price mb-0">2 581 040 сум</p>
+                <p className="priceMonth">337 000 сум x 12 мес</p>
+                <p className="title">
+                  Ноутбук Lenovo V15-IGL N4020 4/256 GB SSD 15.6" HD{" "}
+                </p>
+                <a href="!#" className="btn">
+                  Buy
+                </a>
+              </div>
+            </div>
+            <div className=".col-sm-6 .col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/2022-12-23/noutbuk-lenovo-v15-igl-n4020-4256-gb-ssd-156-hd-185052-0.jpeg"
+                  alt=""
+                />
+                <a href="!#" className="groupName d-block mb-3 mt-3">
+                  Laptops
+                </a>
+                <p className="price mb-0">2 581 040 сум</p>
+                <p className="priceMonth">337 000 сум x 12 мес</p>
+                <p className="title">
+                  Ноутбук Lenovo V15-IGL N4020 4/256 GB SSD 15.6" HD{" "}
+                </p>
+                <a href="!#" className="btn">
+                  Buy
+                </a>
+              </div>
+            </div>
+            <div className=".col-sm-6 .col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/2022-12-23/noutbuk-lenovo-v15-igl-n4020-4256-gb-ssd-156-hd-185052-0.jpeg"
+                  alt=""
+                />
+                <a href="!#" className="groupName d-block mb-3 mt-3">
+                  Laptops
+                </a>
+                <p className="price mb-0">2 581 040 сум</p>
+                <p className="priceMonth">337 000 сум x 12 мес</p>
+                <p className="title">
+                  Ноутбук Lenovo V15-IGL N4020 4/256 GB SSD 15.6" HD{" "}
+                </p>
+                <a href="!#" className="btn">
+                  Buy
+                </a>
+              </div>
+            </div>
+            <div className=".col-sm-6 .col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/2022-12-23/noutbuk-lenovo-v15-igl-n4020-4256-gb-ssd-156-hd-185052-0.jpeg"
+                  alt=""
+                />
+                <a href="!#" className="groupName d-block mb-3 mt-3">
+                  Laptops
+                </a>
+                <p className="price mb-0">2 581 040 сум</p>
+                <p className="priceMonth">337 000 сум x 12 мес</p>
+                <p className="title">
+                  Ноутбук Lenovo V15-IGL N4020 4/256 GB SSD 15.6" HD{" "}
+                </p>
+                <a href="!#" className="btn">
+                  Buy
+                </a>
+              </div>
+            </div>
+            <div className=".col-sm-6 .col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/220x220/products/2022-12-23/noutbuk-lenovo-v15-igl-n4020-4256-gb-ssd-156-hd-185052-0.jpeg"
+                  alt=""
+                />
+                <a href="!#" className="groupName d-block mb-3 mt-3">
+                  Laptops
+                </a>
+                <p className="price mb-0">2 581 040 сум</p>
+                <p className="priceMonth">337 000 сум x 12 мес</p>
+                <p className="title">
+                  Ноутбук Lenovo V15-IGL N4020 4/256 GB SSD 15.6" HD{" "}
+                </p>
+                <a href="!#" className="btn">
+                  Buy
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
